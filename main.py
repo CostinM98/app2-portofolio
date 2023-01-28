f0b150f9707c92c3b3c52c5cffdeb921839667ae
Costin98 <morun.costin@yahoo.com>
2023-01-28 16:34:58 +0200

    create columns,image and descriptions

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit
+import streamlit as st
+# st.set_page_config(layout='wide')
+
+
+col1,col2=st.columns(2)
+
+with col1:
+    st.image('images/photo.png')
+
+with col2:
+    streamlit.title("Morun Costin-Andrei")
+    content="""
+    
+    Seeking a career change to a position involving Software Testing. Driven self-starter and fast learner.
+    
+    
+    """
+
+
+    st.info(content)
+
+content2='''
+
+Below you can find some apps I have built in Python(for it will be only one maybe).Feel free to contact me!
+
+
+
+
+'''
+
+st.write(content2)
